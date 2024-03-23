54ea34834d740a0fdcbd7f3b6c8e182cf463d27e
adg1126 <gutierrez.almiradyan@gmail.com>
2024-03-23 13:24:57 +0800

    Created boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Appbar from './components/Appbar/Appbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Genres from './components/Genres';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <main>
       <Appbar>
-        <div>div</div>
+        <Hero />
+        <Highlights />
+        <Genres />
+        <Footer />
       </Appbar>
     </main>
   );
