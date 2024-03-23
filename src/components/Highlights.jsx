57834b8d4fc1a79895d59759d654e1e282e2fe16
adg1126 <gutierrez.almiradyan@gmail.com>
2024-03-23 19:01:04 +0800

    Styled highlights section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,3 +1,32 @@
+import { useSelector } from 'react-redux';
+import { selectGamesArr } from '../redux/gamesSlice';
+import { Card } from './Card';
+import { Typography } from '@material-tailwind/react';
+
 export default function Highlights() {
-  return <div>Highlights</div>;
+  const gamesArr = useSelector(selectGamesArr);
+
+  return (
+    gamesArr?.length && (
+      <section className='mt-8 flex flex-col items-center gap-y-8'>
+        <div>
+          <Typography
+            variant='h1'
+            color='blue-gray'
+            className='font-bold'
+          >
+            TRENDING GAMES
+          </Typography>
+        </div>
+        <div className='overflow-hidden w-5/6 h-full flex flex-row flex-wrap gap-12 justify-center'>
+          {gamesArr.slice(0, 9).map((g, i) => (
+            <Card
+              key={i}
+              {...g}
+            />
+          ))}
+        </div>
+      </section>
+    )
+  );
 }
