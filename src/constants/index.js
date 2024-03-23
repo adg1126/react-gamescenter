171a353ef678c55d487f6a294056c20d6c0817df
adg1126 <gutierrez.almiradyan@gmail.com>
2024-03-23 14:03:26 +0800

    Stored constants into its own file

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,2 @@
-export const routesArr = ['Home', 'Creators', 'Stores', 'Games'];
+export const routesArr = ['Home', 'Creators', 'Stores', 'Games'],
+  RAWG_URL = 'https://api.escuelajs.co/api/v1/products';
