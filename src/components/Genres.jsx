442c8b8b68773770809654e30dced3ef9bcf9e3a
adg1126 <gutierrez.almiradyan@gmail.com>
2024-03-24 10:47:57 +0800

    Styled, wired up filter by genre and fetch games for genre actions and memoized selectors for genres section

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -1,3 +1,80 @@
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  selectGenresArr,
+  setGenresSectionFilterGenre,
+  selectGenresSectionFilterGenre,
+  fetchGamesByGenre,
+  selectGenresSectionGamesArrFilteredByGenre,
+  selectGamesArr,
+} from '../redux/gamesSlice';
+
+import { Typography, Select, Option } from '@material-tailwind/react';
+import { Card } from './Card';
+import { useEffect } from 'react';
+
 export default function Genres() {
-  return <div>Genres</div>;
+  const genresArr = useSelector(selectGenresArr),
+    filterGenre = useSelector(selectGenresSectionFilterGenre),
+    filteredGamesArr = useSelector(selectGenresSectionGamesArrFilteredByGenre),
+    gamesArr = useSelector(selectGamesArr);
+  const dispatch = useDispatch();
+
+  const handleSetGenre = (genre) => {
+    dispatch(setGenresSectionFilterGenre(genre));
+  };
+
+  useEffect(() => {
+    if (filterGenre?.length > 0) {
+      dispatch(fetchGamesByGenre(filterGenre));
+    }
+  }, [dispatch, filterGenre]);
+
+  return (
+    <section className='mt-32 flex flex-col items-center gap-y-8'>
+      <div className='flex flex-col gap-y-2'>
+        <Typography
+          variant='h1'
+          color='blue-gray'
+          className='font-bold'
+        >
+          FILTER BY GENRES
+        </Typography>
+        <div>
+          {genresArr?.length && (
+            <Select
+              variant='standard'
+              label='Default'
+              onChange={handleSetGenre}
+              value={filterGenre}
+            >
+              {genresArr?.length &&
+                genresArr.map((g, i) => (
+                  <Option
+                    key={i}
+                    value={g.slug}
+                  >
+                    {g.name}
+                  </Option>
+                ))}
+            </Select>
+          )}
+        </div>
+      </div>
+      <div className='overflow-hidden w-5/6 h-full flex flex-row flex-wrap gap-12 justify-center'>
+        {filteredGamesArr?.length > 0 && filterGenre?.length
+          ? filteredGamesArr.slice(0, 9).map((g, i) => (
+              <Card
+                key={i}
+                {...g}
+              />
+            ))
+          : gamesArr.slice(0, 9).map((g, i) => (
+              <Card
+                key={i}
+                {...g}
+              />
+            ))}
+      </div>
+    </section>
+  );
 }
